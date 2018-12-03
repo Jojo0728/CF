adb19703bd96c728546283905ba7e6f67654480c
MSI\Tommy <dyntommy18@gmail.com>
2018-12-03 15:56:48 -0500

    Added UI

---
+++ CyberFight/Assets/UI/script/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+    public GameObject Pausemenu;
+    public GameObject Pausebutton;
+    public GameObject settingmenu;
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        Pausemenu.SetActive(true);
+        Pausebutton.SetActive(false);
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1f;
+        Pausemenu.SetActive(false);
+        Pausebutton.SetActive(true);
+    }
+
+    public void OnSetting()
+    {
+        Time.timeScale = 0;
+        Pausemenu.SetActive(false);
+        settingmenu.SetActive(true);
+    }
+
+    public void OnBack()
+    {
+        Time.timeScale = 0;
+        settingmenu.SetActive(false);
+        Pausemenu.SetActive(true);
+    }
+    public void OnExit()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+}
---
+++ CyberFight/Assets/UI/script/enterlevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class enterlevel : MonoBehaviour {
+    public GameObject homemenu;
+    public GameObject settingmenu;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnStart()
+    {
+        
+        SceneManager.LoadScene("uidemo");
+        
+    }
+
+    public void OnSetting()
+    {
+        homemenu.SetActive(false);
+        settingmenu.SetActive(true);
+
+    }
+
+    public void OnBack()
+    {
+        homemenu.SetActive(true);
+        settingmenu.SetActive(false);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
