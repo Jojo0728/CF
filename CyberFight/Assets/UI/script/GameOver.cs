d474ab57588a15274944398c14fb2095c484a0cd
MSI\Tommy <dyntommy18@gmail.com>
2018-12-04 12:00:31 -0500

    added script for GOv scene

---
+++ CyberFight/Assets/UI/script/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+    public GameObject GameOverc;
+    public GameObject Setting;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnAgain()
+    {
+        SceneManager.LoadScene("uidemo");
+    }
+
+    public void OnExit()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void OnSetting()
+    {
+        Setting.SetActive(true);
+        GameOverc.SetActive(false);
+    }
+
+    public void OnBack()
+    {
+        Setting.SetActive(false);
+        GameOverc.SetActive(true);
+    }
+}
