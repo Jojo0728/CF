7dbb575fe12f61a37d5585327281dfe67233e68c
Jojo0728 <44903961+Jojo0728@users.noreply.github.com>
2018-12-10 11:58:15 -0500

    Enemy spawn in and collider
    In my folder, the 3rd level is being worked on

---
+++ CyberFight/Assets/Jojo/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    private int enemyCount = 0;
+    public int maxEnemyCount = 12;
+    public float timeBetweenSpawns = 2.0f;
+    public GameObject enemy;
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0, timeBetweenSpawns);
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy);
+            //, gameObject.transform.position, gameObject.transform.rotation);
+
+        enemyCount++;
+        if (enemyCount >= maxEnemyCount)
+        {
+            CancelInvoke("Spawn");
+        }
+    }
+}
