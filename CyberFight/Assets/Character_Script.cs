a4aafdb328c34c1b8759c0937ef4aa0ba22167bf
Jojo0728 <44903961+Jojo0728@users.noreply.github.com>
2018-12-12 09:49:48 -0500

    character script
    jojo folder

---
+++ CyberFight/Assets/Character_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Script : MonoBehaviour {
+
+    Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float DashBack = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", DashBack);
+    }
+}
